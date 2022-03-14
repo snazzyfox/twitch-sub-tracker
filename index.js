2612359b7b575666c5dd967f03aa2e5debb8d8f3
SnazzyFox <12706268+snazzyfox@users.noreply.github.com>
2022-03-14 15:01:20 -0700

    Also track bits

--- index.js
+++ index.js
@@ -27,36 +27,37 @@ function getTierName(subTierData) {
 
 let lastGSheetApiCallTime = 0;
 
-async function writeRow(subData) {
-    console.log(JSON.stringify(subData))
+async function writeRow(rowData) {
+    console.log(JSON.stringify(rowData))
     while (Date.now() - lastGSheetApiCallTime < 1000) {  // delay processing until at least 1 sec past last call
         await new Promise(resolve => setTimeout(resolve, 1000));
     }
     lastGSheetApiCallTime = Date.now();
     try {
-        const { spreadsheetId, sheetName } = channels[subData.channel.toLowerCase()];
-        const tierName = getTierName(subData.tier)
+        const { spreadsheetId, sheetName } = channels[rowData.channel.toLowerCase()];
+        const tierName = getTierName(rowData.tier)
         const row = [
             (new Date()).toISOString(),
-            subData.channel,
-            subData.action,
-            getTierName(subData.tier),
-            subData.count,
-            subData.from,
-            subData.to,
-            subData.months,
-            subData.message || '',
+            rowData.channel,
+            rowData.action,
+            getTierName(rowData.tier),
+            rowData.count,
+            rowData.bits,
+            rowData.from,
+            rowData.to,
+            rowData.months,
+            rowData.message || '',
         ]
         const resource = { values: [row] };
-        if (subData.range) {
+        if (rowData.range) {
             const response = (await sheets.spreadsheets.values.update({
-                spreadsheetId, range: subData.range, valueInputOption: 'RAW', resource,
+                spreadsheetId, range: rowData.range, valueInputOption: 'RAW', resource,
             })).data;
         } else {
             const response = (await sheets.spreadsheets.values.append({
                 spreadsheetId, range: sheetName, valueInputOption: 'RAW', resource,
             })).data;
-            subData.range = response.updates.updatedRange;  // this also updates the cache since it's by ref
+            rowData.range = response.updates.updatedRange;  // this also updates the cache since it's by ref
         }
     } catch (err) {
         console.error(err);
@@ -93,12 +94,8 @@ ComfyJS.onGiftSubContinue = (user, sender, extra) => {
     writeRow({
         channel: extra.channel,
         action: 'Gift Continue',
-        tier: null,
-        count: null,
         from: sender,
         to: user,
-        months: null,
-        message: null,
     });
 }
 
@@ -119,8 +116,6 @@ ComfyJS.onSubGift = async (gifterUser, streakMonths, recipientUser, senderCount,
             count: 1,
             from: gifterUser,
             to: recipientUser,
-            months: null,
-            message: null,
         }
     }
     cache.set(originId, subData);
@@ -139,13 +134,20 @@ ComfyJS.onSubMysteryGift = async (gifterUser, numbOfSubs, senderCount, subTierIn
             tier: subTierInfo,
             count: extra.userMassGiftCount,
             from: gifterUser,
-            to: null,
-            months: null,
-            message: null,
         }
     }
     cache.set(originId, subData);
     await writeRow(subData);
+}
+
+ComfyJS.onCheer = ( user, message, bits, flags, extra ) => {
+    writeRow({
+        channel: extra.channel,
+        action: 'Cheer',
+        bits,
+        from: user,
+        message: message,
+    });
 }
 
 ComfyJS.Init(config[0].twitchChannel, null, Object.keys(channels));
